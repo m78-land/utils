5a41d40d0cad8f6b57e4f9f68da5e48ba7fcb307
lxj <1073830130@qq.com>
2019-12-09 21:20:38 +0800

    build: 合并冲突、重新提交

--- dist/index.umd.js
+++ dist/index.umd.js
@@ -466,12 +466,13 @@
 
   /** 获取一个用于挂载Portals或动态弹窗等内容的节点, 多次调用时会获取到相同的节点 */
   var portalsID = 'J__PORTALS__NODE';
-  var getPortalsNode = function getPortalsNode() {
-    var portalsEl = document.getElementById(portalsID);
+  var getPortalsNode = function getPortalsNode(namespace) {
+    var id = portalsID + (namespace ? '__' + namespace : '');
+    var portalsEl = document.getElementById(id);
 
     if (!portalsEl) {
       var el = document.createElement('div');
-      el.id = portalsID;
+      el.id = id;
       el.setAttribute('warning', '⛔⛔HIGH ENERGY⛔⛔');
       portalsEl = document.body.appendChild(el);
     }
