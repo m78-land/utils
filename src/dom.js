5cadbbc796b44b910cd30b73d45ef3de05e5e673
lxj <1073830130@qq.com>
2019-12-09 11:29:44 +0800

    fix: getPortalsNode增加命名空间

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,13 @@
 /** 获取一个用于挂载Portals或动态弹窗等内容的节点, 多次调用时会获取到相同的节点 */
 const portalsID = 'J__PORTALS__NODE';
-export const getPortalsNode = () => {
-  let portalsEl = document.getElementById(portalsID);
+export const getPortalsNode = (namespace) => {
+  const id = portalsID + (namespace ? '__' + namespace : '');
+
+  let portalsEl = document.getElementById(id);
 
   if (!portalsEl) {
     const el = document.createElement('div');
-    el.id = portalsID;
+    el.id = id;
     el.setAttribute('warning', '⛔⛔HIGH ENERGY⛔⛔');
     portalsEl = document.body.appendChild(el);
   }
