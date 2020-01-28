5e1d15087cde9efe14c9228f87bf9c9f851eed43
lxj <1073830130@qq.com>
2020-01-29 02:55:20 +0800

    getPortalsNode namespace转大写

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,7 @@
 /** 获取一个用于挂载Portals或动态弹窗等内容的节点, 多次调用时会获取到相同的节点 */
 const portalsID = 'J__PORTALS__NODE__';
 export const getPortalsNode = (namespace) => {
-  const id = portalsID + (namespace ? namespace : 'DEFAULT');
+  const id = portalsID + (namespace ? namespace.toLocaleUpperCase() : 'DEFAULT');
 
   let portalsEl = document.getElementById(id);
 
